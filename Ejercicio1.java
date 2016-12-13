43439786ac6d2c71fd6e59d0039b25a68d97ec75
Darkjaeger <jesusonazo96@hotmail.com>
2016-12-13 10:29:51 +0100

    new file:   Ejercicio1.java 	new file:   Ejercicio2.java

---
+++ Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+	
+	public static void main (String[] args) {
+		
+		//estructura con for para el incremento desde 2 hasta 10
+		for(int numero1 = 2; numero1 <= 10; numero1++)
+		{
+			System.out.printf("numero es %d%n",numero1); //uso del formateo de numero1 para decimal
+		}
+		
+	}
+}
---
+++ Ejercicio2.java
@@ -0,0 +1,34 @@
+//importacion del la herramienta Scanner
+import java.util.Scanner;
+
+public class Ejercicio2 {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una palabra:");
+		String palabra = sc.next().toLowerCase(); //escaneo de la plabra y comparación entre mayúsculas y minúsculas
+		
+		//uso fallido del switch para localizar tildes en la palabra
+		switch
+		{
+			case : palabra = á (System.out.println("contiene tilde"));
+			break;
+		}
+		{
+			case : palabra = é (System.out.println("contiene tilde"));
+			break;
+		}
+		{
+			case : palabra = í (System.out.println("contiene tilde"));
+			break;
+		}
+		{
+			case : palabra = ó (System.out.println("contiene tilde"));
+			break;
+		}
+		{
+			case : palabra = ú (System.out.println("contiene tilde"));
+			break;
+		}
+	}
+}
